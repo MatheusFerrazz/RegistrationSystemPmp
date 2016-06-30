18dab1b53fac18e9020d3c1d21bf78aff66c4fcb
MatheusFerrazz <matheusb994@gmail.com>
2016-06-30 00:52:35 -0300

    Construção do Controlador

--- RegistrationSystemPmp/src/java/br/com/registrationsystempmp/controle/Controlador.java
+++ RegistrationSystemPmp/src/java/br/com/registrationsystempmp/controle/Controlador.java
@@ -1,12 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.registrationsystempmp.controle;
 
+import br.com.registrationsystempmp.modelo.Funcionario;
+import br.com.registrationsystempmp.persistencia.FuncionarioDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,72 +16,88 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Matheus
  */
-@WebServlet(name = "Controlador", urlPatterns = {"/Controlador"})
+@WebServlet(name = "/Controlador", urlPatterns = {"/Controlador"})
 public class Controlador extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            throws ServletException, IOException, SQLException {
+        //Faz uma busca pela escrita write
+
+        PrintWriter out = response.getWriter();
+        FuncionarioDAO dao = new FuncionarioDAO();
+        String action = request.getParameter("action");
+
+        if (action != null && action.equals("gravar")) {
+            //Busca parametros do request
+            Integer id = 0;
+
+            if (request.getParameter("id") != null && !request.getParameter("id").equals("")) {
+                id = Integer.parseInt(request.getParameter("id"));
+            }
+            String nome = request.getParameter("nome");
+            String secretaria = request.getParameter("secretaria");
+            String vinculo = request.getParameter("vinculo");
+            String setor = request.getParameter("setor");
+            String identificacao = request.getParameter("identificacao");
+            String endereco = request.getParameter("endereco");
+            String numero = request.getParameter("numero");
+            String cidade = request.getParameter("cidade");
+            String telefone = request.getParameter("telefone");
+            String username = request.getParameter("username");
+            String acesso = request.getParameter("acesso");
+
+            Funcionario funcionario = new Funcionario();
+            //funcionario.setIdfuncionario(new Long(id));
+            funcionario.setNome(nome);
+            funcionario.setSecretaria(secretaria);
+            funcionario.setVinculo(vinculo);
+            funcionario.setSetor(setor);
+            funcionario.setIdentificacao(identificacao);
+            funcionario.setEndereco(endereco);
+            funcionario.setNumero(numero);
+            funcionario.setCidade(cidade);
+            funcionario.setTelefone(telefone);
+            funcionario.setUsername(username);
+            funcionario.setAcesso(acesso);
+
+            try {
+                if (id != null && id > 0) {
+                    funcionario.setIdfuncionario(new Long(id));
+                    dao.editar(funcionario);
+
+                } else {
+                    dao.inserir(funcionario);
+                }
+                RequestDispatcher dispatcher = request.getRequestDispatcher("funcionario.jsp");
+                dispatcher.forward(request, response);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } else if (action != null && action.equals("excluir")) {
+            Integer id = 0;
+            if (request.getParameter("id") != null && !request.getParameter("id").equals("")) {
+                id = Integer.parseInt(request.getParameter("id"));
+
+            }
+            Funcionario funcionario = new Funcionario();
+            funcionario.setIdfuncionario(new Long(id));
+
+            try {
+                dao.deletar(funcionario);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("funcionario.jsp");
+                dispatcher.forward(request, response);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("funcionario.jsp");
+            dispatcher.forward(request, response);
         }
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
